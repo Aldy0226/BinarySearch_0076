e605c25438e7b01ba3839c4cf416c1116123e190
Aldy0226 <putra.jaya.ft24@mail.umy.ac.id>
2025-05-07 15:56:36 +0700

    Membuat void display pada binary search

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -51,3 +51,20 @@ void bubbleSortArray()
     } while (pass < nPanjang - 1);
 }
 
+void display()
+{
+    cout << "\n==================================\n";
+    cout << "    element Array Setelah Diurutkan (Asc)    \n";
+    cout << "==================================\n";
+    
+    for (int j = 0; j < nPanjang; j++) // Menampilkan hasil pengurutan array dengan format panah untuk visualisasi urutan
+    {
+        cout << element[j];
+        if (j < nPanjang - 1)
+        {
+            cout << " -> ";
+        }
+    }
+    cout << endl;
+}
+
